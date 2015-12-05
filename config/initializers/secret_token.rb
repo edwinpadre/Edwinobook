504c08279cf4fcde817b0e44a6e435db68d2e425
Ingeniero Edwin Orozco <edwinpadre@hotmail.com>
2015-12-05 16:39:29 -0600

    crear el proyecto y agregado status al mismo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Edwinobook::Application.config.secret_token = '5b257d712618625ae36b4c40288c2d43e36cd4dd935c74751e358c4c70a8cc1d2013161e677451c2d5f96f260c3f2d890f85800530eeb9509b2c627332230894'
